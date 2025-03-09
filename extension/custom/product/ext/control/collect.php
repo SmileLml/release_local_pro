d42a9f85e49cef202f0daf809ed584fc93118665
liumingliang <13693365300@163.com>
2025-03-09 08:51:38 +0000

    * Task for #72798,72799.

--- extension/custom/product/ext/control/collect.php
+++ extension/custom/product/ext/control/collect.php
@@ -4,6 +4,13 @@ class myProduct extends product
 {
     public function collect($product)
     {
+        $browseLink = $this->session->productList ? $this->session->productList : inlink('all');
+
+        if(!$this->app->user->admin and strpos(",{$this->app->user->view->products},", ",$product,") === false and $product != 0)
+        {
+            return print(js::error($this->lang->product->accessDenied) . js::locate($browseLink));
+        }
+
         $product = $this->product->getByID($product);
 
         if(strpos(",{$product->favorites},", ",{$this->app->user->account},") !== false)
@@ -15,6 +22,6 @@ class myProduct extends product
             $product->favorites = rtrim($product->favorites, ',') . ",{$this->app->user->account},";
         }
         $this->dao->update(TABLE_PRODUCT)->set('favorites')->eq($product->favorites)->where('id')->eq($product->id)->exec();
-        return print(js::reload('parent'));
+        return print(js::locate($browseLink));
     }
 }
--- extension/custom/project/ext/control/collect.php
+++ extension/custom/project/ext/control/collect.php
@@ -4,6 +4,13 @@ class myproject extends project
 {
     public function collect($projectID)
     {
+        $browseLink = $this->session->projectList ? $this->session->projectList : inlink('browse');
+
+        if(!$this->app->user->admin and strpos(",{$this->app->user->view->projects},", ",{$projectID},") === false and $projectID != 0)
+        {
+            return print(js::error($this->lang->project->accessDenied) . js::locate($browseLink));
+        }
+
         $project = $this->project->getByID($projectID);
 
         if(strpos(",{$project->favorites},", ",{$this->app->user->account},") !== false)
@@ -15,6 +22,6 @@ class myproject extends project
             $project->favorites = rtrim($project->favorites, ',') . ",{$this->app->user->account},";
         }
         $this->dao->update(TABLE_PROJECT)->set('favorites')->eq($project->favorites)->where('id')->eq($project->id)->exec();
-        return print(js::reload('parent'));
+        return print(js::locate($browseLink));
     }
 }
